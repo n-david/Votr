8daa097f9ccbe4ddff0989ae1fa77e46014c5e85
sharon sw <sharonsuetwli@gmail.com>
2017-03-02 22:15:28 +0000

    Added test routes to render new pages

--- test/test-server.js
+++ test/test-server.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
-app.set('views', __dirname + '/../views/');
+app.set("views", __dirname + "/../views/");
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,9 +20,24 @@ app.post("/", (req, res) => {
 });
 
 /*
+ poll_admin.ejs 
+*/
+app.get("/poll/a/id", (req, res) => {
+  res.render("poll_admin.ejs");
+});
+
+/*
+ poll_voter.ejs 
+*/
+app.get("/poll/v/id", (req, res) => {
+  res.render("poll_voter.ejs");
+});
+
+
+/*
  success_newpoll.ejs 
 */
-app.get("/poll/success", (req, res) => {
+app.get("/poll/create/success", (req, res) => {
   res.render("success_newpoll.ejs");
 });
 
@@ -33,6 +48,15 @@ app.get("/poll/vote/success", (req, res) => {
   res.render("success_votepoll.ejs");
 });
 
+/*
+ already_voted.ejs 
+*/
+app.get("/poll/vote/error", (req, res) => {
+  res.render("already_voted.ejs");
+});
+
+
+
 app.listen(3000, () => {
-  console.log(`Example app listening on port 3000!`);
+  console.log('Example app listening on port 3000!');
 });
