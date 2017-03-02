7b38a7887b213ac5aa37faaecd5a763f9693deef
sharon sw <sharonsuetwli@gmail.com>
2017-03-02 04:11:59 +0000

    Added test-server.js in test/ folder to render ejs files

---
+++ test/test-server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.set("view engine", "ejs");
+app.set('views', __dirname + '/../views/');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+  //res.render(__dirname + '../views/index.ejs');
+});
+
+app.post("/", (req, res) => {
+
+});
+
+app.listen(3000, () => {
+  console.log(`Example app listening on port 3000!`);
+});
