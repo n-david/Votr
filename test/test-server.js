3309c253c3d93d1fbe53449985dd189001336515
Robert Canas <robert_canas@hotmail.com>
2017-03-02 22:04:25 +0000

    Implementation of the mailgun api into the test server. Revision of form action in index.ejs

--- test/test-server.js
+++ test/test-server.js
@@ -9,25 +9,40 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 /*
- index.ejs 
+ index.ejs
 */
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
 app.post("/", (req, res) => {
+  const email = req.body.email
+  var api_key = 'key-80216e96640c6edd8a9e5bb8a9bcaae7';
+  var domain = 'sandboxa721143bbca24e14bca66e736c6fdfb9.mailgun.org';
+  var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 
+  var data = {
+    from: 'Votr <postmaster@sandboxa721143bbca24e14bca66e736c6fdfb9.mailgun.org>',
+    to: email,
+    subject: 'Votr Admin/Voter Links',
+    text: `Testing some Mailgun awesomness! \n Admin url: adminURL \n Voter url: voterURL`
+  };
+
+  mailgun.messages().send(data, function (error, body) {
+    console.log(body);
+  });
+  res.redirect("/poll/create/success");
 });
 
 /*
- success_newpoll.ejs 
+ success_newpoll.ejs
 */
 app.get("/poll/success", (req, res) => {
   res.render("success_newpoll.ejs");
 });
 
 /*
- success_votepoll.ejs 
+ success_votepoll.ejs
 */
 app.get("/poll/vote/success", (req, res) => {
   res.render("success_votepoll.ejs");
