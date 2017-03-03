24486ccbd92330e8ca7253914bea2433b4f10146
Robert Canas <robert_canas@hotmail.com>
2017-03-03 20:40:59 +0000

    Revision on app.js, Expansion of description.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,47 +1,67 @@
-//document on ready:
-$(() => {
+$(document).ready(function() {
 
-  
-  // $.ajax({
-  //   method: "GET",
-  //   url: "/api/users"
-  // }).done((users) => {
-  //   for(user of users) {
-  //     $("<div>").text(user.name).appendTo($("body"));
-  //   }
-  // });;
+let choiceCounter = 2;
+
+  function createChoiceElement() {
+
+    choiceCounter++;
+
+    const $choiceForm = $("<article>").addClass("form-group choice-form-group");
+
+    const $choiceNumber = $("<label>").prepend("Choice " + choiceCounter + ": ");
+
+    const $div = $("<div>").addClass("choice-inputs");
+    const $choiceTextBox = $("<input>").addClass("form-control");
+    // const $deleteChoice = $(`
+    //   <span class='delete-choice'>
+    //     <i class='fa fa-trash-o' aria-hidden='true'></i>
+    //   </span>`);
+    const $deleteChoice = $("<span>").addClass("delete-choice").html(`<i class="fa fa-trash-o" aria-hidden="true"></i>`);
+
+    const $describeExpand = $("<small>").addClass("describe-expand2").html(`<p>Expand description box</p>`);
+
+
+    // const $expandedDescription = $("<div>").addClass("choice-describe");
+    const $expandedDescription = $(`
+      <div class="choice-describe">
+      <label for="choice-description">Description:</label>
+      <textarea type="text" name="choice-description" class="form-control" rows="3"></textarea>
+      </div>
+      `)
+
+    //const $labelDescription = $("<label>").text("Description: ");
+    //const $descriptionChoice = $("<textarea>").addClass("form-control");
+
+    $div.append($choiceTextBox, $deleteChoice);
+    //$expandedDescription.append($labelDescription, $descriptionChoice);
+
+    $choiceForm.append($choiceNumber, $div, $describeExpand, $expandedDescription);
+
+    return $choiceForm;
+  }
+
+  $('.fa-plus').on('click', function() {
+    console.log("click");
+    createChoiceElement().appendTo($('.choice-container-created'));
+    expand2();
+  });
+
+
+  function expand() {
+    $(".describe-expand").on("click", function() {
+    console.log("click");
+    //const $descriptionBox = $();
+    $(this).siblings('.choice-describe').slideToggle('fast');
+    })
+  }
+
+  function expand2(){
+      $(".describe-expand2").on("click", function() {
+    console.log("click");
+    $(this).siblings('.choice-describe').slideToggle('fast');
+    })
+  }
+
+  expand();
+
 });
-
-// $(document).ready(function() {
-
-//   function createChoiceElement(choice) {
-
-//     const $choiceForm = $("<article>").addClass("form-group choice-form-group");
-
-//     const $choiceNumber = $("label[for='choice']").text("Choice");
-
-//     const $div = $("<div>").addClass("choice-inputs");
-//     const $choiceTextBox = $("<input>").addClass("form-control");
-//     const $deleteChoice = $("<span>").addClass("delete-choice");
-//     const $describeExpand = $("<small>").addClass("describe-expand").text("Expand description");
-
-//     const $expandedDescription = $
-
-
-//   }
-
-// });
-
-          // <article class="form-group choice-form-group">
-          //   <label for="choice">Choice 1:</label>
-          //   <div class="choice-inputs">
-          //     <input type="text" name="choice" class="form-control">
-          //     <span class="delete-choice"><i class="fa fa-trash-o" aria-hidden="true"></i></span>
-          //   </div>
-          //   <small id="describe-expand" class="form-text text-muted"><a href="#">Expand description box</a></small>
-          //   <div class="choice-describe">
-          //     <label for="choice-description">Description:</label>
-          //     <textarea type="text" name="choice-description" class="form-control" rows="3"></textarea>
-          //   </div>
-          // </article>
-
