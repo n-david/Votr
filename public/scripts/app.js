3839c764518aa3367c67fb7f356eceaa4ab06629
sharon sw <sharonsuetwli@gmail.com>
2017-03-03 02:04:04 +0000

    Deleted test.css ref in ejs files

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,13 @@
+//document on ready:
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+
+  
+  // $.ajax({
+  //   method: "GET",
+  //   url: "/api/users"
+  // }).done((users) => {
+  //   for(user of users) {
+  //     $("<div>").text(user.name).appendTo($("body"));
+  //   }
+  // });;
 });
