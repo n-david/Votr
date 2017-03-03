75ce1a89dd7f37b16695269b9dc00652f797e790
Robert Canas <robert_canas@hotmail.com>
2017-03-03 02:26:10 +0000

    Revision of index.ejs for the multiple id's. Commented out the app.js script to avoid conflict with the unfinished code.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,3 +8,37 @@ $(() => {
     }
   });;
 });
+
+// $(document).ready(function() {
+
+//   function createChoiceElement(choice) {
+
+//     const $choiceForm = $("<article>").addClass("form-group choice-form-group");
+
+//     const $choiceNumber = $("label[for='choice']").text("Choice");
+
+//     const $div = $("<div>").addClass("choice-inputs");
+//     const $choiceTextBox = $("<input>").addClass("form-control");
+//     const $deleteChoice = $("<span>").addClass("delete-choice");
+//     const $describeExpand = $("<small>").addClass("describe-expand").text("Expand description");
+
+//     const $expandedDescription = $
+
+
+//   }
+
+// });
+
+          // <article class="form-group choice-form-group">
+          //   <label for="choice">Choice 1:</label>
+          //   <div class="choice-inputs">
+          //     <input type="text" name="choice" class="form-control">
+          //     <span class="delete-choice"><i class="fa fa-trash-o" aria-hidden="true"></i></span>
+          //   </div>
+          //   <small id="describe-expand" class="form-text text-muted"><a href="#">Expand description box</a></small>
+          //   <div class="choice-describe">
+          //     <label for="choice-description">Description:</label>
+          //     <textarea type="text" name="choice-description" class="form-control" rows="3"></textarea>
+          //   </div>
+          // </article>
+
