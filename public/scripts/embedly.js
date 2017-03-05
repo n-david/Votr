a1279196b09f2ccf9492a6cad7c07df88d0f5876
sharon sw <sharonsuetwli@gmail.com>
2017-03-05 10:18:48 +0000

    Access Control restriction. Not yet working ocde

---
+++ public/scripts/embedly.js
@@ -0,0 +1,31 @@
+//rendering choice-info-block
+
+$(document).ready(function() {
+
+  //sidebar popup on click
+  $(".choice").on("click", function(event) {
+    $(this).closest(".voter-info").find(".choice-info-block").show("slide", {direction : "right"}, 300);
+    const choiceTitle = $(this).html();
+    const choiceDescription = $(this).data("description");
+    const sidebar = $(this).closest(".voter-info").find(".choice-info-block");
+    sidebar.find("h2").html(choiceTitle);
+
+    if (checkIfUrl(choiceDescription)) {
+      $.ajax({
+      method: 'HEAD',
+      url: input,
+      success: function() {
+        const embedJSON = $.getJSON('https://api.embedly.com/1/oembed?' + $.param({
+                            url: choiceDescription,
+                            key: "d37e5b7ef8754516a19d57a2bb857cf4"
+                          }));
+        console.log(embedJSON);
+      },
+      error: function() {
+        console.log("not valid url");
+        sidebar.find("p").html(choiceDescription);
+      }
+      });
+    }
+  });
+});
--- public/scripts/test-app.js
+++ public/scripts/test-app.js
@@ -1,16 +1,42 @@
 $(document).ready(function() {
 
-  //sidebar popup on click
-  $(".choice").on("click", function(event) {
+    $(".choice").on("click", function(event) {
     $(this).closest(".voter-info").find(".choice-info-block").show("slide", {direction : "right"}, 300);
     const choiceTitle = $(this).html();
     const choiceDescription = $(this).data("description");
     const sidebar = $(this).closest(".voter-info").find(".choice-info-block");
-
     sidebar.find("h2").html(choiceTitle);
-    sidebar.find("p").html(choiceDescription);
 
+    if (checkIfUrl(choiceDescription)) {
+      $.ajax({
+      method: 'GET',
+      url: choiceDescription,
+      success: function() {
+        const embedJSON = $.getJSON('https://api.embedly.com/1/oembed?' + $.param({
+                            url: choiceDescription,
+                            key: "d37e5b7ef8754516a19d57a2bb857cf4"
+                          }));
+        console.log(embedJSON);
+      },
+      error: function() {
+        console.log("not valid url");
+        sidebar.find("p").html(choiceDescription);
+      }
+      });
+    }
   });
+
+  //sidebar popup on click
+  // $(".choice").on("click", function(event) {
+  //   $(this).closest(".voter-info").find(".choice-info-block").show("slide", {direction : "right"}, 300);
+  //   const choiceTitle = $(this).html();
+  //   const choiceDescription = $(this).data("description");
+  //   const sidebar = $(this).closest(".voter-info").find(".choice-info-block");
+
+  //   sidebar.find("h2").html(choiceTitle);
+  //   sidebar.find("p").html(choiceDescription);
+
+  // });
 
   //sort ol into array on 'vote button' clicked
   $(".vote-btn").on("click", function(event) {
